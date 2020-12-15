bccc12a754b66ad822a92ffc92c78d3bc8e54fa5
vishwas-prabhu <vishwas.prabhu@robosoftin.com>
2020-12-15 19:06:25 +0530

    added error handling div to the FPW component
    FPW - forgot password

--- src/app/login/components/login-forgot-password/login-forgot-password.component.ts
+++ src/app/login/components/login-forgot-password/login-forgot-password.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginForgotPasswordComponent implements OnInit {
 
+  email = { value: '', error: false };
+
   constructor() { }
 
   ngOnInit(): void {
