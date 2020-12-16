a6653ae0fd08a503bc6005f05ce786eb44f30772
Sharath Kumar <72862431+sharathkumar-kr@users.noreply.github.com>
2020-12-16 15:23:47 +0530

    Dev/login user type (#3)
    * Added User Type Selection Basic Design
    
    * Added User Modes and Controls
    
    * Updated user choice font and icon size
    
    * Updated Icons
    
    * Updated Icons on Hover, Changed SVG
    
    * Added Selected Tick Icon,Updated User Select Logic
    
    * Cleaned Up Styles and Added Comments
    
    Co-authored-by: sharathkumar106 <sharathkr606@gmail.com>

--- src/app/login/components/login-user-type/login-user-type.component.ts
+++ src/app/login/components/login-user-type/login-user-type.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-user-type.component.scss']
 })
 export class LoginUserTypeComponent implements OnInit {
-
+  selectedItem = 0;
+  userTypes = [
+    'Recruiter',
+    'Organizer',
+    'Approval Authority'
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectUserType(id: number): void {
+    this.selectedItem = id;
+    console.log(this.userTypes[this.selectedItem]);
+  }
 }
