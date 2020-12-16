3502752d2fb50db583cc05264d0dd5afb080578f
vishwas-prabhu <vishwas.prabhu@robosoftin.com>
2020-12-16 10:08:19 +0530

    username, email and mobile no. validation added

--- src/app/shared/services/form-validation.service.ts
+++ src/app/shared/services/form-validation.service.ts
@@ -8,8 +8,23 @@ export class FormValidationService {
   constructor() { }
 
   isValidRobosoftEmail(email: string): boolean {
-    const regEx = /^[a-zA-Z]+(\.[a-zA-Z]+)?@robosoftin.com$/;
-    return regEx.test(email);
+    const regEx = /^[a-zA-Z]+(\.[a-zA-Z]+)?@robosoftin\.com$/;
+    return regEx.test(email.trim());
+  }
+
+  isValidEmail(email: string): boolean {
+    const regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+    return regEx.test(email.trim());
+  }
+
+  isValidUsername(username: string): boolean {
+    const regEx = /([a-z]{1,}\s[a-z]{3,})|([a-z]{3,}\s[a-z]{1,})|([a-z]{3,})/;
+    return regEx.test(username.trim());
+  }
+
+  isValidMobileNumber(mobileNo: string): boolean {
+    const regEx = /^(\+?[0-9]{1,3}\s?)?[0-9]{3}[\s|-]?[0-9]{3}[\s|-]?[0-9]{4}$/;
+    return regEx.test(mobileNo.trim());
   }
 
 }
