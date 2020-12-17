9c6e10738090062b77914e89b4052d5bee7bc677
Swathi N Shayana <55015685+Swathi-N-Shayana@users.noreply.github.com>
2020-12-17 19:59:59 +0530

    Added Applicant form and success page

--- src/app/applicant/components/form-one/form-one.component.ts
+++ src/app/applicant/components/form-one/form-one.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormValidationService } from 'src/app/shared/services/form-validation.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-one',
@@ -7,9 +10,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormOneComponent implements OnInit {
 
-  constructor() { }
+  
+  //Reactive Forms
+  signupForm: FormGroup;
+  formError = false;
+
+  constructor(
+    private validation: FormValidationService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email, this.isEmailValid]),
+      'mobile': new FormControl(null, Validators.required),
+      'designation': new FormControl(null, Validators.required),
+      'radio': new FormControl('recruiter'),
+      'password': new FormControl(null, [Validators.required, this.isPasswordValid.bind(this)]),
+      'rePassword': new FormControl(null, [Validators.required, this.isPasswordMatching.bind(this)]),
+    });
+
+    this.signupForm.setValue({
+      'name': '',
+      'email': '',
+      'mobile': '',
+      'designation': '',
+      'radio': 'recruiter',
+      'password': '',
+      'rePassword': ''
+    })
   }
 
+  onSubmit(): void {
+    console.log(this.signupForm);
+    if (this.signupForm.invalid) {
+      this.formError = true;
+      return;
+    }
+    this.router.navigate(['../form'], { relativeTo: this.route });
+  }
+
+  isEmailValid(control: FormControl): { [s: string]: boolean } {
+    if (!control.value) {
+      return { 'invalidCompanyEmail': true };
+    }
+    if (this.validation.isValidRobosoftEmail(control.value)) {
+      return null;
+    }
+  }
+
+  isPasswordValid(control: FormControl): { [s: string]: boolean } {
+    if (!control.value) {
+      return { 'invalidPassword': true };
+    }
+    if (this.validation.isValidPassword(control.value)) {
+      return null;
+    }
+  }
+
+  isPasswordMatching(control: FormControl): { [s: string]: boolean } {
+    if (
+      !control.value ||
+      !this.signupForm.get('password').value ||
+      this.signupForm.get('password').value !== control.value
+    ) {
+      return { 'passwordMatchError': true }
+    }
+    return null;
+  }
 }
