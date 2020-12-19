92968e78e243d3200ef6edafdf2fc7ef8239784a
sharathkumar-kr <sharathkumar.kr@robosoftin.com>
2020-12-19 19:17:58 +0530

    Added Reactive Form Control, Fixed Form Style

--- src/app/applicant/components/form-two/form-two.component.ts
+++ src/app/applicant/components/form-two/form-two.component.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,15 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-two.component.scss']
 })
 export class FormTwoComponent implements OnInit {
-  showErrors = true;
   applicantFormTwo: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.applicantFormTwo = new FormGroup({
+      workHistoryForm: new FormArray([
+        new FormGroup({
+          companyName: new FormControl(''),
+          position: new FormControl(''),
+          from: new FormControl(''),
+          to: new FormControl(''),
+          location: new FormControl('')
+        })
+      ]),
+      educationForm: new FormArray([
+        new FormGroup({
+          instituitionName: new FormControl(''),
+          grade: new FormControl(''),
+          from: new FormControl(''),
+          to: new FormControl(''),
+          location: new FormControl('')
+        })
+      ]),
+      addressForm: new FormGroup({
+        address: new FormControl(''),
+        state: new FormControl(''),
+        pincode: new FormControl('')
+      })
+    });
+
+    console.log(this.applicantFormTwo.controls);
   }
 
   get form(): any {
     return this.applicantFormTwo.controls;
+  }
+
+  get workHistory(): any {
+    return this.applicantFormTwo.get('workHistoryForm');
+  }
+
+  addCompanyForm(): void {
+    const formGroup = new FormGroup({
+      companyName: new FormControl(''),
+      position: new FormControl(''),
+      from: new FormControl(''),
+      to: new FormControl(''),
+      location: new FormControl('')
+    });
+
+    this.workHistory.push(formGroup);
   }
 
   onSubmit(): void {
