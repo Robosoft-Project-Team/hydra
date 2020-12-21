59f19362bfe7df215ca1c2f85ff20caf5be1cd4e
Swathi N Shayana <55015685+Swathi-N-Shayana@users.noreply.github.com>
2020-12-21 10:48:50 +0530

    Added CV form

--- src/app/applicant/applicant.module.ts
+++ src/app/applicant/applicant.module.ts
@@ -1,13 +1,15 @@
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 
+
 import { ApplicantRoutingModule } from './applicant-routing.module';
 import { ApplicantComponent } from './applicant.component';
 import { FormOneComponent, FormTwoComponent, FormThreeComponent, ApplySuccessComponent } from './components';
+import { DragDirective } from './directives/drag.directive';
 
 
 @NgModule({
-  declarations: [ApplicantComponent, FormOneComponent, FormTwoComponent, FormThreeComponent, ApplySuccessComponent],
+  declarations: [ApplicantComponent, FormOneComponent, FormTwoComponent, FormThreeComponent, ApplySuccessComponent, DragDirective],
   imports: [
     ApplicantRoutingModule,
     SharedModule
