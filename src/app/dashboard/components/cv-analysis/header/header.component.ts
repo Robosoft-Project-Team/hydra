5ec2990a31be19e3e51a1ff240ce28f673cb20be
sharathkumar-kr <sharathkumar.kr@robosoftin.com>
2020-12-24 21:19:12 +0530

    Fixed Date Picker - added moment.js format

--- src/app/dashboard/components/cv-analysis/header/header.component.ts
+++ src/app/dashboard/components/cv-analysis/header/header.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDatepicker, MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import * as moment from 'moment';
+import { FormControl } from '@angular/forms';
 
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }]
 })
 export class HeaderComponent implements OnInit {
+
+  filterDate = new FormControl(moment(new Date()));
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedDate(event: MatDatepickerInputEvent<moment.Moment>): void {
+    console.log(event.value.format('DD/MM/YYYY'));
   }
 
 }
