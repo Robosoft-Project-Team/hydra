f2fb5951a6ef55d75c5be4a8c8d9b8b8c541236a
vishwas-prabhu <vishwas.prabhu@robosoftin.com>
2020-12-28 11:27:49 +0530

    styled cv details component

--- src/app/dashboard/components/cv-details/cv-details.component.ts
+++ src/app/dashboard/components/cv-details/cv-details.component.ts
@@ -7,6 +7,8 @@ import {
   animate,
   transition,
 } from '@angular/animations';
+import { CvAnalysisService } from '../../services/cv-analysis.service';
+import { Applicant } from '../../shared/interface';
 
 @Component({
   selector: 'app-cv-details',
@@ -18,7 +20,16 @@ import {
         right: '0%'
       })),
       transition('* => open', [
-        style({right: '-10%'}),
+        style({ right: '-5%' }),
+        animate('0.2s')
+      ]),
+    ]),
+    trigger('overlay', [
+      state('show', style({
+        opacity: 1
+      })),
+      transition('* => show', [
+        style({ opacity: 0 }),
         animate('0.5s')
       ]),
     ]),
@@ -26,23 +37,45 @@ import {
 })
 export class CvDetailsComponent implements OnInit {
 
-  id = 0;
+  applicant: Applicant;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private cv: CvAnalysisService
   ) { }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params.id;
+    const id = +this.route.snapshot.params.id;
+    this.applicant = this.cv.getApplicant(id);
   }
 
   goBack(): void {
-    this.router.navigate(['../'], { relativeTo: this.route});
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   dontGo(): void {
     console.log('no go');
+  }
+
+  calculateAge(birthday): number {
+    const birthdayArray = birthday.split('/');
+    const birthDate = birthdayArray[1] + '-' + birthdayArray[0] + '-' + birthdayArray[2];
+    const age = Date.now() - new Date(birthDate).getTime();
+    const ageDate = new Date(age);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  diff(from, to): string {
+    const fromDateArray = from.split('-');
+    const toDateArray = to.split('-');
+    const monthDiff = +toDateArray[1] - +fromDateArray[1];
+    const yearDiff = +toDateArray[0] - +fromDateArray[0];
+    if (monthDiff < 0) {
+      return `${yearDiff - 1} y ${monthDiff + 12} m`;
+    } else {
+      return `${yearDiff} y ${monthDiff} m`;
+    }
   }
 
 }
--- src/app/dashboard/components/cv-details/left-sidebar/left-sidebar.component.ts
+++ src/app/dashboard/components/cv-details/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-sidebar.component.scss']
 })
 export class LeftSidebarComponent implements OnInit {
+
+  @Input() applicant;
 
   constructor() { }
 
