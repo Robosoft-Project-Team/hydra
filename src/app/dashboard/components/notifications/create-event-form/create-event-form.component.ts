a3c85529fb6a0002cf7cd9d0d940301cf6b4c369
sharathkumar-kr <sharathkumar.kr@robosoftin.com>
2021-01-03 23:33:44 +0530

    Fixed Form Alignment and Datepicker Formats

--- src/app/dashboard/components/notifications/create-event-form/create-event-form.component.ts
+++ src/app/dashboard/components/notifications/create-event-form/create-event-form.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-// import { MatDatepicker } from '@angular/material/datepicker';
-// import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD MMM YYYY',
+  },
+  display: {
+    dateInput: 'DD MMM YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-create-event-form',
   templateUrl: './create-event-form.component.html',
-  styleUrls: ['./create-event-form.component.scss']
+  styleUrls: ['./create-event-form.component.scss'],
+  providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }]
 })
 export class CreateEventFormComponent implements OnInit {
 
@@ -14,7 +25,7 @@ export class CreateEventFormComponent implements OnInit {
     '2.00',
     '3.00'
   ];
-  
+
   constructor() { }
 
   ngOnInit(): void {
